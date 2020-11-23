098e2f964e75e10c5edf20d98c1061c5815cc24c
Maryia Ondra <maryiaondra@gmail.com>
2020-11-23 15:14:29 +0300

    fixed code

---
+++ FlappyBirdGame/Assets/Scripts/Bird/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BirdMover))]
+public class Bird : MonoBehaviour
+{
+    private int _score;
+    private BirdMover _mover;
+
+    private void Start()
+    {
+        _mover = GetComponent<BirdMover>();   
+    }
+
+    public void ResetPlayer() 
+    {
+        _score = 0;
+        _mover.ResetBird();
+    }
+
+    public void Die()
+    {
+        Debug.Log("I AM DEAD!");
+        Time.timeScale = 0;
+    }
+}
---
+++ FlappyBirdGame/Assets/Scripts/Bird/BirdCollisionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Bird))]
+public class BirdCollisionHandler : MonoBehaviour
+{
+    private Bird _bird;
+
+    private void Start()
+    {
+        _bird = GetComponent<Bird>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _bird.Die();   
+    }
+}
